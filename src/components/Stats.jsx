1a28e125468b16bb178059da17379acf42c9c4b2
Muhammed <faraanshaik19@gmail.com>
2025-05-02 00:03:48 +0530

    commit

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -3,12 +3,15 @@ import React from "react";
 const Stats = ({ items }) => {
   console.log(items);
 
+  const packed = items.filter((item) => item.packed).length;
+  // console.log(packed);
+
   return (
     <footer className="stats">
       {items.length > 0 ? (
         <em>
           You have {items.length} {items.length > 1 ? "items" : "item"} on your
-          list, and you already packed
+          list, and you already packed {packed}
         </em>
       ) : (
         <em>Start adding some items to your packing list</em>
