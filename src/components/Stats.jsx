547ce6f27c19d18892abf9c93a049f1bac654b98
Muhammed <faraanshaik19@gmail.com>
2025-05-07 00:16:04 +0530

    stats commit

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 
 const Stats = ({ items }) => {
+  let packed = items.filter((item) => item.packed).length;
+  // console.log(packed);
+  const percentage = Math.round((packed / items.length) * 100);
+  console.log(percentage);
+
   return (
     <footer className="stats">
       {items.length > 0 ? (
-        <em>{items.length} have been packed</em>
+        <em>
+          You have {items.length} items on your list, and you have packed{" "}
+          {packed} ({percentage}%)
+        </em>
       ) : (
         <em>Start adding some items to your packing list</em>
       )}
