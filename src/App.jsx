f00edc0ac02c40ed089a6ad48da078ce2d9760a9
Muhammed <faraanshaik19@gmail.com>
2025-05-09 23:59:50 +0530

    app and form commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import Logo from "./components/Logo";
 import Form from "./components/Form";
 import PackingList from "./components/PackingList";
 import Stats from "./components/Stats";
+import { useState } from "react";
 
 const App = () => {
+  const [items, setItems] = useState([]);
   return (
     <>
       <Logo />
       <Form />
-      <PackingList />
+      <PackingList item={items} />
       <Stats />
     </>
   );
