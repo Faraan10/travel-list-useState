a16838453e560ab5c3d8df988083eb848f0cf42c
Muhammed <faraanshaik19@gmail.com>
2025-05-09 23:36:26 +0530

    app commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,45 +2,14 @@ import Logo from "./components/Logo";
 import Form from "./components/Form";
 import PackingList from "./components/PackingList";
 import Stats from "./components/Stats";
-import { useState } from "react";
 
 const App = () => {
-  const [items, setItems] = useState([]);
-  console.log(items);
-
-  const handleAddItems = (data) => {
-    // console.log(data);
-    setItems((prevItems) => [...prevItems, data]);
-  };
-
-  const handleToggle = (id) => {
-    setItems((prevItems) =>
-      prevItems.map((item) =>
-        item.id === id ? { ...item, packed: !item.packed } : item
-      )
-    );
-  };
-  // console.log(items);
-
-  const handleDelete = (id) => {
-    // console.log(id);
-    setItems((prevItems) => prevItems.filter((item) => item.id !== id));
-  };
-
-  const handleClear = () => {
-    setItems([]);
-  };
   return (
     <>
       <Logo />
-      <Form handleAddItems={handleAddItems} />
-      <PackingList
-        items={items}
-        handleToggle={handleToggle}
-        handleDelete={handleDelete}
-        handleClear={handleClear}
-      />
-      <Stats items={items} />
+      <Form />
+      <PackingList />
+      <Stats />
     </>
   );
 };
