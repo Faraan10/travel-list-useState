b80850d94c13bad5b6384a4642e5db0b0497ae11
Muhammed <faraanshaik19@gmail.com>
2025-05-10 00:00:03 +0530

    form commit

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Form = () => {
+  const [form, setForm] = useState({
+    id: "",
+    description: "",
+    quantity: "",
+    packed: false,
+  });
+
+  const handleChange = () => {};
+
+  const handleSubmit = () => {};
+
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
       <select>
-        {/* {Array.from({ length: 20 }, (_, i) => i + 1).map((item) => (
-          <option key={item} value={item}>
-            {item}
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((number) => (
+          <option value={number} key={number}>
+            {number}
           </option>
-        ))} */}
+        ))}
       </select>
       <input type="text" placeholder="Item..." />
       <button type="submit">Add</button>
