5d4981d4b203fe796fbacf8152480612c09a9d86
Muhammed <faraanshaik19@gmail.com>
2025-05-24 21:04:00 +0530

    lidt commit

--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -1,44 +1,17 @@
-import React, { useState } from "react";
-import Item from "./Item";
+import React from "react";
 
-const PackingList = ({ items, toggleItem, deleteItem, clearList }) => {
-  let sortedItems = items;
-
-  const [sortBy, setSortBy] = useState("input");
-
-  if (sortBy === "input") {
-    sortedItems = items;
-  }
-
-  if (sortBy === "description") {
-    sortedItems = items
-      .slice()
-      .sort((a, b) => a.description.localeCompare(b.description));
-  }
-
-  if (sortBy === "packed") {
-    sortedItems = items.slice().sort((a, b) => a.packed - b.packed);
-  }
+const PackingList = () => {
   return (
     <div className="list">
-      <ul>
-        {sortedItems?.map((item) => (
-          <Item
-            key={item.id}
-            item={item}
-            toggleItem={toggleItem}
-            deleteItem={deleteItem}
-          />
-        ))}
-      </ul>
+      <ul></ul>
 
       <div className="actions">
-        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
-          <option value="input">Sort by input order</option>
-          <option value="description">Sort by description</option>
-          <option value="packed">Sort by packed status</option>
+        <select>
+          <option>Sort by input order</option>
+          <option>Sort by description</option>
+          <option>Sort by packed status</option>
         </select>
-        <button onClick={clearList}>Clear list</button>
+        <button>Clear list</button>
       </div>
     </div>
   );
