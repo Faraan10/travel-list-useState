5349357183cf8c08028843023d79df81144aa091
Muhammed <faraanshaik19@gmail.com>
2025-05-24 21:52:06 +0530

    form commit

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
+
 const Form = () => {
   return (
     <form className="add-form">
       <h3>What do you need for your 😍 trip?</h3>
       <select>
-        {Array.from({ length: 20 }, (_, i) => i + 1).map((number) => (
-          <option></option>
-        ))}
+        {/* {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option key={num} value={num}>
+            {num}
+          </option>
+        ))} */}
       </select>
       <input type="text" placeholder="Item..." />
       <button type="submit">Add</button>
