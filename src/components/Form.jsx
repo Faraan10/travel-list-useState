3c273bad59b1178eccb1826287409eed310394e4
Muhammed <faraanshaik19@gmail.com>
2025-05-25 22:44:58 +0530

    form commit

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,11 +5,9 @@ const Form = () => {
     <form className="add-form">
       <h3>What do you need for your 😍 trip?</h3>
       <select>
-        {/* {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
-          <option key={num} value={num}>
-            {num}
-          </option>
-        ))} */}
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option key={num}>{num}</option>
+        ))}
       </select>
       <input type="text" placeholder="Item..." />
       <button type="submit">Add</button>
