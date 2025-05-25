838cb3d45321a7e83b3d8ca73e4406243675390f
Muhammed <faraanshaik19@gmail.com>
2025-05-25 23:07:26 +0530

    app commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,19 @@ import Logo from "./components/Logo";
 import Form from "./components/Form";
 import PackingList from "./components/PackingList";
 import Stats from "./components/Stats";
+import { useState } from "react";
 
 const App = () => {
+  const [items, setItems] = useState([]);
+  console.log(items);
+
+  const addItems = (data) => {
+    setItems((prevItems) => [...prevItems, data]);
+  };
   return (
     <>
       <Logo />
-      <Form />
+      <Form addItems={addItems} />
       <PackingList />
       <Stats />
     </>
