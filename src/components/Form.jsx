44471043719e81620a950660a5ac7f19a7ba3648
Muhammed <faraanshaik19@gmail.com>
2025-05-25 22:55:08 +0530

    form commit

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,51 @@
 import { useState } from "react";
 
 const Form = () => {
+  const [data, setData] = useState({
+    id: "",
+    description: "",
+    quantity: 1,
+    packed: false,
+  });
+
+  const { id, description, quantity, packed } = data;
+
+  const handleChange = (e) => {
+    setData((prevItems) => ({
+      ...prevItems,
+      [e.target.name]:
+        e.target.name == "quantity" ? Number(e.target.value) : e.target.value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!description.trim()) {
+      return;
+    }
+
+    const newData = {
+      id: crypto.randomUUID(),
+    };
+    console.log(data);
+  };
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-      <select>
+      <select name="quantity" onChange={handleChange}>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
-          <option key={num}>{num}</option>
+          <option value={num} key={num}>
+            {num}
+          </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        name="description"
+        value={description}
+        onChange={handleChange}
+      />
       <button type="submit">Add</button>
     </form>
   );
