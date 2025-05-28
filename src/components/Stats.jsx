63004aaefdc0eaed4e8f47f5b390ae573df4b4fd
Muhammed <faraanshaik19@gmail.com>
2025-05-28 13:05:11 +0530

    stats commit

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-const Stats = () => {
-  return <footer className="stats">"Jello"</footer>;
+const Stats = ({ items }) => {
+  const packedItems = items.filter((item) => item.packed).length;
+
+  return (
+    <footer className="stats">
+      {items.length == 0
+        ? "Start adding some items to your packing list 🚀"
+        : `You have ${items.length} items on your list and you already packed ${packedItems}`}
+    </footer>
+  );
 };
 
 export default Stats;
