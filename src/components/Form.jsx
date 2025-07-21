6e32df01d404095f157c1c4a62b6424389e8cb54
Muhammed <faraanshaik19@gmail.com>
2025-07-21 23:35:13 +0530

    form commit

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,44 @@
+import { useState } from "react";
+
 const Form = () => {
+  const [info, setInfo] = useState({
+    id: "",
+    description: "",
+    packed: false,
+    quantity: 1,
+  });
+
+  const { id, description, packed, quantity } = info;
+
+  const handleChange = (e) => {
+    setInfo({
+      ...info,
+      [e.target.name]:
+        e.target.name === "quantity" ? Number(e.target.value) : e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(info);
+  };
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-      <select></select>
-      <input type="text" placeholder="Item..." name="description" />
+      <select name="quantity" value={quantity} onChange={handleChange}>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((i) => (
+          <option key={i} value={i}>
+            {i}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        name="description"
+        value={description}
+        onChange={handleChange}
+      />
       <button type="submit">Add</button>
     </form>
   );
